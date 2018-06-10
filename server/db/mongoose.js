5d1797a7ae89f3b86dcdfc101e9c933dc38601cc
harshTTH <harshrai456@outlook.com>
2018-06-11 00:28:32 +0530

    mongoose connect to mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const uri = `mongodb://TTH:9301461020h@ds231245.mlab.com:31245/node-todo-db`;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(uri);
 
 module.export = {
     mongoose
