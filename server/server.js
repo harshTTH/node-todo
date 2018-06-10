5e900bb6de4ada49582164fb3b81a517d06a811d
harshTTH <harshrai456@outlook.com>
2018-06-10 15:04:17 +0530

    restrutured modals with /todo route POST

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./modals/Todo');
+const {Users} = require('./modals/Users');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    const todo = new Todo({
+        text:req.body.text
+    });
+
+    todo.save().then((doc)=>{
+        res.send(doc);
+    },(err)=>{
+        res.status(400).send(err);
+    })
+})
+
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log('Server running!');
+})
